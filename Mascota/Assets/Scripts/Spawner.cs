866f6d90a6113f268a51ba4640344b648b106045
Diego Suarez <diegosuarezlopezalfonso@gmail.com>
2022-12-09 12:51:39 +0000

    en proceso

--- Mascota/Assets/Scripts/Spawner.cs
+++ Mascota/Assets/Scripts/Spawner.cs
@@ -2,16 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jugador : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
-
-
-
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    // Update is called once per frame
     void Update()
     {
         
